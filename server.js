f9f79d8ae5345b65f13da2b6632792cb6b5e8797
Ayush Verma <ayushv1895@gmail.com>
2018-02-21 19:31:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+app.get('/article-one', function (req, res) {
+  res.sendFile("Article 1 requested and served here");
+});
+
+app.get('/article-two', function (req, res) {
+  res.sendFile("Article 2 requested and served here");
+});
+
+app.get('/article-three', function (req, res) {
+  res.sendFile("Article 3 requested and served here");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
