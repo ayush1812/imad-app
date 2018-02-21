bda768c62fc46ab1d48efd6c97993d0026ef362f
Ayush Verma <ayushv1895@gmail.com>
2018-02-21 20:23:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,9 +9,78 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var articleone={
+    title:"Article 1",
+    heading:"Article One",
+    date:"21,02 2018",
+    content:` <p>
+            Content inside html page.
+            Content inside html page.
+            Content inside html page.
+            Content inside html page.
+            Content inside html page.
+            Content inside html page.
+        </p>
+        <p>
+            Content inside html page.
+            Content inside html page.
+            Content inside html page.
+            Content inside html page.
+            Content inside html page.
+            Content inside html page.
+        </p>
+        <p>
+            Content inside html page.
+            Content inside html page.
+            Content inside html page.
+            Content inside html page.
+            Content inside html page.
+            Content inside html page.
+        </p>`
+};
+
+function crteateTemplate(data){
+    
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+    
+var htmlTemplate = `
+<html>
+<head>
+    <title>
+        ${title}
+    </title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+      <link href="/ui/style.css" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="container">
+    <div>
+        <a href="/">Home</a>
+    </div>
+    <hr/>
+    <h3>
+        ${heading}
+    </h3>
+    <div>
+       $date}
+    </div>
+    <div>
+        ${content}
+    </div>
+    </div>
+</body>
+</html>
+
+`;
+return htmlTemplate;
+}
 
 app.get('/article-one', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articleOne))
 });
 
 app.get('/article-two', function (req, res) {
